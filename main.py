c7be2e5c59faf6b37ff2cab18c1c7d504c3de332
Jes94 <102390824+Jes94@users.noreply.github.com>
2022-03-29 11:49:36 -0400

    Added lists for destinations, restaurants, transportation, and entertainment.

---
+++ main.py
@@ -0,0 +1,4 @@
+destination_list = ['New York', 'Los Angeles', 'Vegas', 'Orlando', 'Tampa', 'Charlotte', 'San Antonio', 'San Jose', 'Chicago', 'Dallas', 'Philadelphia']
+restaurant_list = ['McDonalds', 'Burger King', 'Wendys', 'Taco Bell', 'Qdoba', 'Chipotle', 'Olive Garden', 'TGI Fridays', 'Applebees', 'Logans Roadhouse']
+transportation_list = ['Rental car', 'Uber', 'Lyft', 'Taxi', 'Limo service', 'Bus']
+entertainment_list = ['going to the movies', 'visiting a local theme park', 'going golfing', 'visiting an arcade', 'going on a local tour', 'going to a concert', 'visting a museum', 'visiting a zoo', 'going clubbing']
